015e038e88097a0df7fef3f9b746b1b2f5047431
Viktor Poszt <vposzt@gmail.com>
2024-12-28 18:49:20 +0100

    feature: do game 2d array

--- Fonxy_Tetris/Fonxy_Tetris/main.cpp
+++ Fonxy_Tetris/Fonxy_Tetris/main.cpp
@@ -1,9 +1,13 @@
 #include <raylib.h>
+#include "grid.h"
 
 int main() {
 	InitWindow(300, 600, "raylib Tetris");
 	Color darkBlue = { 44, 44, 127, 255 };
 	SetTargetFPS(60);
+
+	Grid grid = Grid();
+	grid.Print();
 
 	while (WindowShouldClose() == false) {
 		BeginDrawing();
